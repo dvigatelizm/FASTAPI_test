2ca1c52f5b5c8f64ee25252d35ac2f746fcd6c1f
r2d2 <valiulliner@rambler.ru>
2025-12-24 00:13:43 +0500

    fix

--- main.py
+++ main.py
@@ -6,21 +6,22 @@ app = FastAPI(title='To-Do-Serv')
 class Task(BaseModel):
     title: str
     completed: bool = False
-    deleted: bool = False
+    deleted: bool = False 
 
-tasks = {}
-
+# Имитация БД
+tasks = {} # Пустой словарь с тасками
+# 'task1' : ['Сделать ДЗ по ПИ', True]
 task_id_counter = 1
+# POST /tasks – создание новой задачи
 
-# POST /task
 @app.post("/tasks")
 def create_task(task: Task):
     global task_id_counter
     tasks[task_id_counter] = task
     task_id_counter += 1
-    return {"id": task_id_counter-1, "task": task}
-
-# GET /tasks
+    return {"id": task_id_counter - 1, "task": task}
+    
+# GET /tasks – получение списка всех задач
 @app.get("/tasks")
 def get_all_tasks():
     active = {
@@ -28,37 +29,29 @@ def get_all_tasks():
         if not task.deleted
     }
     return {"tasks": active}
+# GET /tasks/{id} – получение информации о конкретной задаче
 
-# GET /Tasks/{id}
-@app.get("/tasks/{task_id}")
+@app.get("/tasks/{task_id}") # GET / tasks/3 => get_task(task_id=5)
 def get_task(task_id: int):
-    if task_id not in tasks:
+    if task_id not in tasks: # Есть ли задача с таким ID?
         raise HTTPException(status_code=404, detail="Task not found")
     return tasks[task_id]
 
-# PUT /tasks/{id}
+# PUT /tasks/{id} – обновление задачи (изменение текста или статуса)
 @app.put("/tasks/{task_id}")
+#  PUT/tasks/1
+# {'title' : "Купить хлеб И КЕФИР" , 'completed': true}
 def update_task(task_id: int, updated_task: Task):
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="Task for update not found")
     tasks[task_id] = updated_task
     return {"id": task_id, "task": updated_task}
 
-# DELETE /task/{id}
-@app.delete("/task/{task_id}")
+# DELETE /tasks/{id} – удаление задачи
+@app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
     if task_id not in tasks:
         raise HTTPException(status_code=404, detail="Task for delete not found")
     # del tasks[task_id]
     tasks[task_id].deleted = True
-    #return {"id": task_id, "status": "deleted"}
     return {"status": "deleted"}
-
-# GET /tasks/deleted
-@app.get("/tasks/deleted")
-def get_delete_tasks():
-    deleted = {
-        task_id: task for task_id, task in tasks.items()
-        if tasks.deleted
-    }
-    return {"deleted_tasks": {deleted}}
